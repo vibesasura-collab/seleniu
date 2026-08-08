660176082b7c2ccc2ea5104c8ba9e400168fd2fc
vibesasura-collab <vibesasura@gmail.com>
2026-08-08 21:45:50 +0530

    Update Newone.java

--- src/main/java/Newone.java
+++ src/main/java/Newone.java
@@ -112,7 +112,7 @@ public class Newone {
                         if (!number.isEmpty()) {
                             int cost = Integer.parseInt(number);
 
-                            if (cost <= 30) {
+                            if (cost <= 40) {
                                 passBtn.click();
                                 System.out.println("Clicked 'Pass now for " + cost + "'");
                                 sleep(300);
